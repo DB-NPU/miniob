f5d3b7745654b5fc448e2af3d0b3c25d8a76e466
luooofan <luooofan@qq.com>
2023-11-03 23:10:22 +0800

    impl lruk cache

--- deps/common/lang/lruk_cache.h
+++ deps/common/lang/lruk_cache.h
@@ -10,6 +10,7 @@ See the Mulan PSL v2 for more details. */
 
 #pragma once
 
+#include <cassert>
 #include <functional>
 #include <set>
 #include <array>
@@ -17,62 +18,9 @@ See the Mulan PSL v2 for more details. */
 
 namespace common {
 
-// npu-exp: 请不要修改**模板声明**和**类内 public 方法** 其他可以随意改动
 template <size_t K, typename Key, typename Value, typename Hash = std::hash<Key>, typename Pred = std::equal_to<Key>>
 class LruKCache {
 
-public:
-  LruKCache(size_t reserve = 0)
-  {
-    if (reserve > 0) {
-      searcher_.reserve(reserve);
-    }
-  }
-
-  ~LruKCache()
-  {
-    destroy();
-  }
-
-  void destroy()
-  {
-    for (ListNode *node : searcher_) {
-      delete node;
-    }
-    searcher_.clear();
-  }
-
-  size_t count() const
-  {
-    return 0;
-  }
-
-  bool get(const Key &key, Value &value)
-  {
-    return false;
-  }
-
-  void put(const Key &key, const Value &value)
-  {
-  }
-
-  void remove(const Key &key)
-  {
-  }
-
-  // 只要遍历到每一个 <k,v> 即可，没有任何顺序要求
-  void foreach(std::function<bool(const Key &, const Value &)> func)
-  {
-  }
-
-  // 以 LRU-K 序遍历每一个 <k, v>
-  // NOTE: 这里的函数名字并不合理，只是为了与 LruCache 保持一致
-  void foreach_reverse(std::function<bool(const Key &, const Value &)> func)
-  {
-  }
-
-private:
-  // npu-exp: 以下只是示例 同学们可以自行更改
   class ListNode {
   public:
     Key key_;
@@ -83,6 +31,44 @@ private:
   public:
     ListNode(const Key &key, const Value &value) : key_(key), value_(value)
     {}
+    void access(size_t cur_time)
+    {
+      history_[times_++ % K] = cur_time;
+    }
+    // 符合 LRU-K 序
+    bool operator< (const ListNode& other) const
+    {
+      if (this->times_ < K && other.times_ < K) {
+        // 都没有访问够 K 次
+        // LRU
+        return this->get_last_access() < other.get_last_access();
+        // FIFO
+        // return this->get_first_access() < other.get_first_access();
+      } else if (this->times_ < K) {
+        return true;
+      } else if (other.times_ < K) {
+        return false;
+      } else {
+        // 都访问够 K 次
+        return this->get_kth_prev_access() < other.get_kth_prev_access();
+      }
+    }
+  private:
+    size_t get_last_access() const
+    {
+      assert(times_ > 0 && times_ < K); // 只能对访问次数不足 K 次的节点调用此函数
+      return history_[times_ - 1];
+    }
+    size_t get_first_access() const
+    {
+      assert(times_ > 0 && times_ < K); // 只能对访问次数不足 K 次的节点调用此函数
+      return history_[0];
+    }
+    size_t get_kth_prev_access() const
+    {
+      assert(times_ >= K); // 只能对访问次数为 K 次的节点调用此函数
+      return history_[times_ % K];
+    }
   };
 
   class PListNodeHasher {
@@ -126,11 +112,110 @@ private:
     }
   };
 
+public:
+  LruKCache(size_t reserve = 0)
+  {
+    if (reserve > 0) {
+      searcher_.reserve(reserve);
+    }
+  }
+
+  ~LruKCache()
+  {
+    destroy();
+  }
+
+  void destroy()
+  {
+    for (ListNode *node : searcher_) {
+      delete node;
+    }
+    searcher_.clear();
+    cache_.clear();
+  }
+
+  size_t count() const
+  {
+    return searcher_.size();
+  }
+
+  bool get(const Key &key, Value &value)
+  {
+    auto iter = searcher_.find((ListNode *)&key);
+    if (iter == searcher_.end()) {
+      return false;
+    }
+
+    lruk_touch(*iter);
+    value = (*iter)->value_;
+    return true;
+  }
+
+  void put(const Key &key, const Value &value)
+  {
+    auto iter = searcher_.find((ListNode *)&key);
+    if (iter != searcher_.end()) {
+      ListNode *ln = *iter;
+      ln->value_ = value;
+      lruk_touch(ln);
+      return;
+    }
+
+    ListNode *ln = new ListNode(key, value);
+    lruk_push(ln);
+  }
+
+  void remove(const Key &key)
+  {
+    auto iter = searcher_.find((ListNode *)&key);
+    if (iter != searcher_.end()) {
+      lruk_remove(*iter);
+    }
+  }
+
+  // 只要遍历到每一个 <k,v> 即可，没有任何顺序要求
+  void foreach(std::function<bool(const Key &, const Value &)> func)
+  {
+    foreach_reverse(func);
+  }
+
+  // 以 LRU-K 序遍历每一个 <k, v>
+  // NOTE: 这里的函数名字并不合理，只是为了与 LruCache 保持一致
+  void foreach_reverse(std::function<bool(const Key &, const Value &)> func)
+  {
+    for (auto iter = cache_.begin(); iter != cache_.end(); ++iter) {
+      bool ret = func((*iter)->key_, (*iter)->value_);
+      if (!ret) {
+        break;
+      }
+    }
+  }
+
 private:
-  // npu-exp: 以下只是示例 同学们可以自行更改
+  void lruk_touch(ListNode* node)
+  {
+    cache_.erase(node);
+    node->access(cur_time_++);
+    cache_.insert(node);
+  }
+  void lruk_push(ListNode* node)
+  {
+    searcher_.insert(node);
+    node->access(cur_time_++);
+    cache_.insert(node);
+  }
+  void lruk_remove(ListNode *node)
+  {
+    cache_.erase(node);
+    searcher_.erase(node);
+    delete node;
+  }
+
+private:
   using CacheType = std::set<ListNode *, PListNodeComparator>;
   using SearchType = std::unordered_set<ListNode *, PListNodeHasher, PListNodePredicator>;
 
+private:
   size_t cur_time_{0};
   CacheType cache_; // real cache. maintain LRU-K order
   SearchType searcher_; // impl O(1) find
